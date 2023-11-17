b85933a6125351b2ee4342d5bee05fadb830d157
BabkenK <139691909+BabkenK@users.noreply.github.com>
2023-11-18 01:05:25 +0400

    Գրել ծրագիր, որը օգտվողին թույլ կտա մուտքագրել թիվ և պարզել արդյո՞ք թիվը պարզ է, թե ոչ։
    Գրել ծրագիր, որը օգտվողին թույլ կտա մուտքագրել թիվ և պարզել արդյո՞ք թիվը պարզ է, թե ոչ։

---
+++ question 14.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num = 0; 
+    int i = 0; 
+    int chk = 0;
+    cout<<"Enter a Number: ";
+    cin>>num;
+    for(i=2; i<num; i++)
+    {
+        if(num%i==0)
+        {
+            chk++;
+            break;
+        }
+    }
+    if(chk==0)
+        cout<<"\nIt is a Prime Number";
+    else
+        cout<<"\nIt is not a Prime Number";
+    cout<<endl;
+    return 0;
+}
